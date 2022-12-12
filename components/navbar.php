e2eab6e073485e84947d1f1e5fda62a9d82ba821
GM202108 <91591210+GM202108@users.noreply.github.com>
2022-12-12 11:23:09 -0500

    Add files via upload
    Crear una carpeta llamada "gm" dentro de esa carpeta crear otra carpeta llamada "Site" y dentro de Site meter todos los archivos de git

---
+++ components/navbar.php
@@ -0,0 +1,36 @@
+<div class="container ">
+    <header
+        class="d-flex align-items-center justify-content-center  py-3 mb-2">
+
+        <a href="<?php echo baseUrl ; ?>">
+            <img src="./Imagenes/Logo_Okami_SinLT.svg" alt="" style="width: 100px;" style="height: 100px;">
+        </a>
+      </header>
+
+        <ul class="nav justify-content-center border-bottom py-3 mb-4">
+        <li>
+            <a href="<?php echo baseUrl ; ?>" class="px-2 btn btn-danger m-1 tp-style-nav">Inicio<a>
+        </li>
+
+        <li>
+            <a href="<?php echo baseUrl . '/comic.php'; ?>" class="px-2 btn btn-danger m-1 tp-style-nav">Cómic</a>
+        </li>
+
+        <li>
+            <a href="<?php echo baseUrl . '#Historia'; ?>"class="px-2 btn btn-danger m-1 tp-style-nav">Historia</a>
+        </li>
+
+        <li>
+            <a href="<?php echo baseUrl . '#Personajes'; ?>"class="px-2 btn btn-danger m-1 tp-style-nav">Personajes</a>
+        </li>
+
+        <li>
+            <a href="<?php echo baseUrl . '#Contacto'; ?>"class="px-2 btn btn-danger m-1 tp-style-nav">Contacto</a>
+        </li>
+
+        <li>
+            <a href="<?php echo baseUrl . '#Acercade'; ?>"class="px-2 btn btn-danger m-1 tp-style-nav">Acerca de</a>
+        </li>
+
+        </ul>
+</div>
